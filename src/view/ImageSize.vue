9bf4f0c853d0d37da9442a913bcc4bf28e5a8cd9
pengz <2972564513@qq.com>
2021-10-15 17:21:54 +0800

    fix: vue实现商品缩放功能完善

--- src/view/ImageSize.vue
+++ src/view/ImageSize.vue
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import { quillEditor } from 'vue-quill-editor'
+// import { quillEditor } from 'vue-quill-editor'
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 import 'quill/dist/quill.bubble.css';
